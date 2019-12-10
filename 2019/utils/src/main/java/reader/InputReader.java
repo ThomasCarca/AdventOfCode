c2748c3494af30f3d4967a509b4e94f8c7811aa2
Thomas Boulbes <tboulbes@ippon.fr>
2019-12-10 09:53:12 +0100

    Added comma-separated input reading feature in InputReader

--- 2019/utils/src/main/java/reader/InputReader.java
+++ 2019/utils/src/main/java/reader/InputReader.java
@@ -5,15 +5,26 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class InputReader {
 
-    public static List<Integer> readFromPath(String filePath) throws IOException {
+    public static List<Integer> readFromPathWithLineBreaks(String filePath) throws IOException {
         ArrayList<Integer> input = new ArrayList<>();
         List<Integer> readLines = Files.readAllLines(Paths.get(filePath), StandardCharsets.UTF_8)
                 .stream()
+                .mapToInt(Integer::parseInt)
+                .mapToObj(Integer::valueOf)
+                .collect(Collectors.toList());
+        return readLines;
+    }
+
+    public static List<Integer> readFromPathWithCommas(String filePath) throws IOException {
+        ArrayList<Integer> input = new ArrayList<>();
+        List<Integer> readLines = Arrays.stream(Files.readString(Paths.get(filePath), StandardCharsets.UTF_8)
+                .split(","))
                 .mapToInt(Integer::parseInt)
                 .mapToObj(Integer::valueOf)
                 .collect(Collectors.toList());
--- 2019/utils/src/test/java/reader/InputReaderShould.java
+++ 2019/utils/src/test/java/reader/InputReaderShould.java
@@ -1,9 +1,8 @@
 package reader;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
-import reader.InputReader;
 
 import java.io.IOException;
 import java.nio.file.NoSuchFileException;
@@ -15,28 +14,54 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 @DisplayName("InputReader should")
 public class InputReaderShould {
 
-    @Test
-    @DisplayName("read the input file entirely")
-    public void testReadTheInputFileEntirely() throws IOException {
-        String filePath = "src/test/java/reader/input";
-        List<Integer> readInput = InputReader.readFromPath(filePath);
-        assertEquals(4, readInput.size());
-        assertEquals(1, readInput.get(0));
-        assertEquals(22, readInput.get(1));
-        assertEquals(333, readInput.get(2));
-        assertEquals(4444, readInput.get(3));
+    @Nested
+    @DisplayName("on linebreak-separated input")
+    class OnLineBreakSeparatedInput {
+
+        private final String filePath = "src/test/java/reader/line-break-input";
+
+        @Test
+        @DisplayName("read the input file entirely")
+        public void testReadTheInputFileEntirely() throws IOException {
+            List<Integer> readInput = InputReader.readFromPathWithLineBreaks(filePath);
+            assertEquals(4, readInput.size());
+            assertEquals(1, readInput.get(0));
+            assertEquals(22, readInput.get(1));
+            assertEquals(333, readInput.get(2));
+            assertEquals(4444, readInput.get(3));
+        }
+
+        @Test
+        @DisplayName("throw a NoSuchFileException exception if the file does not exist")
+        public void throwsNoSuchFileExceptionIfNoFileFound() {
+            final String filePath = "src/test/filethatdoesnotexist";
+            assertThrows(NoSuchFileException.class, () -> InputReader.readFromPathWithLineBreaks(filePath));
+        }
     }
 
-    @Test
-    @DisplayName("throw a NoSuchFileException exception if the file does not exist")
-    public void throwsNoSuchFileExceptionIfNoFileFound() throws IOException {
-        final String filePath = "src/test/filethatdoesnotexist";
-        assertThrows(NoSuchFileException.class, new Executable() {
-            @Override
-            public void execute() throws Throwable {
-                InputReader.readFromPath(filePath);
-            }
-        });
+    @Nested
+    @DisplayName("on comma-separated input ")
+    class OnCommaSeparatedInput {
+
+        private String filePath = "src/test/java/reader/comma-input";
+
+        @Test
+        @DisplayName("read the input file entirely")
+        public void testReadTheInputFileEntirely() throws IOException {
+            List<Integer> readInput = InputReader.readFromPathWithCommas(filePath);
+            assertEquals(4, readInput.size());
+            assertEquals(1, readInput.get(0));
+            assertEquals(22, readInput.get(1));
+            assertEquals(333, readInput.get(2));
+            assertEquals(4444, readInput.get(3));
+        }
+
+        @Test
+        @DisplayName("throw a NoSuchFileException exception if the file does not exist")
+        public void throwsNoSuchFileExceptionIfNoFileFound() {
+            final String filePath = "src/test/filethatdoesnotexist";
+            assertThrows(NoSuchFileException.class, () -> InputReader.readFromPathWithCommas(filePath));
+        }
     }
 
 
